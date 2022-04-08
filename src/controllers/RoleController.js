ab5c400b36e3d532b180c216258368f1f464753b
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-04-09 03:18:49 +0600

    roleListWithPermissions query done

--- src/controllers/RoleController.js
+++ src/controllers/RoleController.js
@@ -1,3 +1,5 @@
+const { json } = require('express/lib/response');
+const Permission = require('../models/Permission');
 const Role = require('../models/Role');
 const RoleHasPermission = require('../models/RoleHasPermission');
 const UserHasRole = require('../models/UserHasRole');
@@ -147,92 +149,49 @@ module.exports = class RoleController{
     }
 
 
-     //All role list 
-     static roleListwithPermissios = async(req, res)=>{
-      try{
-          const roleListwithPermissios = await Role.find().lean().exec();
-          return res.status(200).json({
-            code: 200,
-            message: "role List",
-            data: roleListwithPermissios,
-          });
-
-      }catch(error){
-        res.status(501).json({
-          code: 501,
-          message: error.message,
-          error: true,
-        });
-      }
-    }
-
+     
     //All role list with their permissions List 
     static roleListWithPermissions = async(req, res)=>{
-      //return console.log("hello")
-      try {
-        //const roleWisePermissionShow = await RoleHasPermission.find().exec();
-      //   const roleWisePermissionShow = await RoleHasPermission.aggregate([{
-      //     $lookup: {
-      //         from: "roles", // collection name in db
-      //         localField: "roleId",
-      //         foreignField: "_id",
-      //         as: "roleInformation"
-      //     }
-      // }]).exec();
+        try {
+          //Fetch role_has_permission collection/table and also joind role collection/table. becasue role_has_permission collection has single roleId.
+          const roleWithPermission = await RoleHasPermission.aggregate([{
+          $lookup: {
+              from: "roles", // collection name in db
+              localField: "roleId",
+              foreignField: "_id",
+              as: "roleInformation"
+              }
+            }]
+          ).exec();
 
-
-      const roleWisePermissionShow = await db.RoleHasPermission.aggregate([
-
-        // Join with user_info table
-        {
-            $lookup:{
-                from: "roles",       // other table name
-                localField: "roleId",   // name of users table field
-                foreignField: "_id", // name of userinfo table field
-                as: "role_info"         // alias for userinfo table
+          //1st gess roleWithPermissionArray has null array
+          let roleWithPermissionArray = []
+          for(var i=0; roleWithPermission.length > i ; i++){
+              //2nd Seperate permission Array
+              let permissionQueryArray = roleWithPermission[i].permissionId;
+              //3rd permission array info is null
+                  let permissionArray = []
+                  //4th permissionQueryArray is also another array. so again loop for another query to fetch permission info into permisson collection
+                  for(var j=0; permissionQueryArray.length > j; j++){
+                    let {_id:permisiionId, name:permissionName, groupName }  = await Permission.findOne({_id:permissionQueryArray[j]})
+                    let permissionInfoObject = {permisiionId, permissionName, groupName}
+                    //5th permission all info push into permissionArray
+                    permissionArray.push(permissionInfoObject)
+                  }
+            //6th destructuring role information
+            let {_id:roleId, name:roleName} = roleWithPermission[i].roleInformation[0];
+           // 7th permissionArray and destructuring role information data build a new object
+            let totalObject = {
+                roleInfo:{roleId, roleName},
+                permissionArray
             }
-        },
-        {   $unwind:"$role_info" },     // $unwind used for getting data in object or for one record only
-    
-        // Join with user_role table
-        {
-            $lookup:{
-                from: "permissions", 
-                localField: "permissionId", 
-                foreignField: "_id",
-                as: "permission_info"
-            }
-        },
-        {   $unwind:"$permission_info" },
-    
-        // define some conditions here 
-        // {
-        //     $match:{
-        //         $and:[{"userName" : "admin"}]
-        //     }
-        // },
-    
-        // define which fields are you want to fetch
-        // {   
-        //     $project:{
-        //         _id : 1,
-        //         email : 1,
-        //         userName : 1,
-        //         userPhone : "$user_info.phone",
-        //         role : "$user_role.role",
-        //     } 
-        // }
-    ]);
-
-
-
-
-
-
+            //8th this new object push into roleWithPermissionArray
+            roleWithPermissionArray.push(totalObject);
+          }
         return res.status(200).json({
           code: 200,
           message: "All role list with their permissions List",
-          data: roleWisePermissionShow,
+          data: roleWithPermissionArray,
         });
       } catch (error) {
         res.status(501).json({
